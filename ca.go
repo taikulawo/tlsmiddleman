624dae7bbd0e5f161be61679e4c1d4e3159570cb
wuweichao <wuweichao.1@bytedance.com>
2020-08-30 18:12:44 +0800

    保存

--- ca.go
+++ ca.go
@@ -9,33 +9,48 @@ import (
 	"io/ioutil"
 	"math/big"
 	"net"
+	"os"
 	"time"
 )
 
 type Certificate struct {
 	certificate *x509.Certificate
-	privateKey *PrivateKey
+	derBytes    []byte
+	privateKey  *PrivateKey
 }
 
+func (this *Certificate) pemBlock() *pem.Block {
+	return &pem.Block{
+		Type:  PEM_HEADER_OF_CERTIFICATE,
+		Bytes: this.derBytes,
+	}
+}
+
+func (this *Certificate) WriteToFile(path string) error {
+	file, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+	if err != nil {
+		return err
+	}
+	return pem.Encode(file, this.pemBlock())
+}
 
 type CertificateAuthority struct {
-	config *RuntimeConfig
-	tlsConfig *TLSConfig
-	privateKey *PrivateKey
+	config      *RuntimeConfig
+	tlsConfig   *TLSConfig
+	privateKey  *PrivateKey
 	certificate *Certificate
 }
 
-
-func NewCA(config *RuntimeConfig, tlsConfig *TLSConfig) *CertificateAuthority{
+func NewCA(config *RuntimeConfig, tlsConfig *TLSConfig) *CertificateAuthority {
 	ca := &CertificateAuthority{
-		config: config,
+		config:    config,
 		tlsConfig: tlsConfig,
 	}
-	panic(ca.generateSelfSignCertificate())
+	ca.generateSelfSignCertificate()
 	return ca
 }
 
-func (this *CertificateAuthority) LoadPkFromFile(filePath string) (*PrivateKey,error){
+func (this *CertificateAuthority) LoadPkFromFile(filePath string) (*PrivateKey, error) {
 	data, err := ioutil.ReadFile(filePath)
 	if err != nil {
 		return nil, err
@@ -51,38 +66,7 @@ func (this *CertificateAuthority) LoadPkFromFile(filePath string) (*PrivateKey,e
 }
 
 func (this *CertificateAuthority) LoadCertificateFromFile(path string) (*Certificate, error) {
-
-}
-
-func (this *CertificateAuthority) CreatedCertificateFor(organization, host string, isCA bool, issuer *Certificate) (*Certificate, error) {
-	template := &x509.Certificate{
-		SerialNumber: new(big.Int).SetInt64(int64(time.Now().UnixNano())),
-		Subject: pkix.Name{
-			Organization: []string{organization},
-			CommonName: host,
-		},
-		DNSNames: []string{host},
-		NotBefore: time.Now().AddDate(0,-1,0),
-		NotAfter: time.Now().Add(time.Hour * 24 * 365),
-		KeyUsage: x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
-	}
-	if ip := net.ParseIP(host); ip != nil {
-		template.IPAddresses = []net.IP{ip}
-	}
-	if (isCA) {
-		template.IsCA = true
-		template.KeyUsage = template.KeyUsage | x509.KeyUsageCertSign
-	}
-	isSelfSign := issuer == nil
-	if isSelfSign {
-		template.ExtKeyUsage = [] x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth | x509.ExtKeyUsageClientAuth}
-	}
-}
-
-// 如果自签CA证书，pkOfClient == pk
-// 如果为client签发那么 pkOfClient是重新生成的密钥对，pk是CA的密钥对
-func (this *CertificateAuthority) doCreateCertificate(template, issuer *x509.Certificate, pkOfClient *PrivateKey,pk *PrivateKey) (*Certificate, error){
-	certBytes, err := x509.CreateCertificate(rand.Reader,template,issuer,pkOfClient.rsa.Public(),pk.rsa)
+	certBytes, err := ioutil.ReadFile(path)
 	if err != nil {
 		return nil, err
 	}
@@ -92,7 +76,57 @@ func (this *CertificateAuthority) doCreateCertificate(template, issuer *x509.Cer
 	}
 	return &Certificate{
 		certificate: cert,
-		privateKey: pkOfClient,
+		derBytes:    certBytes,
+		privateKey:  nil,
+	}, nil
+}
+
+func (this *CertificateAuthority) CreatedCertificateFor(organization, host string, isCA bool, issuer *x509.Certificate, pk *PrivateKey) (*Certificate, error) {
+	template := &x509.Certificate{
+		SerialNumber: new(big.Int).SetInt64(int64(time.Now().UnixNano())),
+		Subject: pkix.Name{
+			Organization: []string{organization},
+			CommonName:   host,
+		},
+		DNSNames:  []string{host},
+		NotBefore: time.Now().AddDate(0, -1, 0),
+		NotAfter:  time.Now().Add(time.Hour * 24 * 365),
+		KeyUsage:  x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
+	}
+	if ip := net.ParseIP(host); ip != nil {
+		template.IPAddresses = []net.IP{ip}
+	}
+	if isCA {
+		template.IsCA = true
+		template.KeyUsage = template.KeyUsage | x509.KeyUsageCertSign
+	}
+	isSelfSign := issuer == nil
+	clientPair, err := this.GeneratePrivateKey(2048)
+	if err != nil {
+		return nil, err
+	}
+	if isSelfSign {
+		template.ExtKeyUsage = []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth | x509.ExtKeyUsageClientAuth}
+	}
+
+	return this.doCreateCertificate(template, issuer, clientPair, pk)
+}
+
+// 如果自签CA证书，pkOfClient == pk
+// 如果为client签发那么 pkOfClient是重新生成的密钥对，pk是CA的密钥对
+func (this *CertificateAuthority) doCreateCertificate(template, issuer *x509.Certificate, pkOfClient *PrivateKey, pk *PrivateKey) (*Certificate, error) {
+	certBytes, err := x509.CreateCertificate(rand.Reader, template, issuer, pkOfClient.rsa.Public(), pk.rsa)
+	if err != nil {
+		return nil, err
+	}
+	cert, err := x509.ParseCertificate(certBytes)
+	if err != nil {
+		return nil, err
+	}
+	return &Certificate{
+		certificate: cert,
+		derBytes:    certBytes,
+		privateKey:  pkOfClient,
 	}, nil
 }
 
@@ -101,32 +135,34 @@ func (this *CertificateAuthority) createTemplateFor(organization string, dnsName
 		SerialNumber: new(big.Int).SetInt64(int64(time.Now().UnixNano())),
 		Subject: pkix.Name{
 			Organization: []string{organization},
-			CommonName: dnsName,
+			CommonName:   dnsName,
 		},
-		DNSNames: []string{dnsName},
-		NotBefore: time.Now().AddDate(0,-1,0),
-		NotAfter: time.Now().Add(time.Hour * 24 * 365),
-		KeyUsage: x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
+		DNSNames:  []string{dnsName},
+		NotBefore: time.Now().AddDate(0, -1, 0),
+		NotAfter:  time.Now().Add(time.Hour * 24 * 365),
+		KeyUsage:  x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
 	}
 }
 
-func (this *CertificateAuthority) generateSelfSignCertificate() error {
+func (this *CertificateAuthority) generateSelfSignCertificate() {
 	var err error
 	if this.privateKey, err = this.LoadPkFromFile(this.tlsConfig.CAPrivateKeyFilePath); err != nil {
 		if this.privateKey, err = this.GeneratePrivateKey(this.tlsConfig.KeyLen); err != nil {
 			panic(err)
 		}
-		return this.privateKey.WriteToFile(this.tlsConfig.CAPrivateKeyFilePath)
+		Must(this.privateKey.WriteToFile(this.tlsConfig.CAPrivateKeyFilePath))
 	}
 	if this.certificate, err = this.LoadCertificateFromFile(this.tlsConfig.CACertificateFilePath); err != nil {
-		if this.certificate, err = this.CreatedCertificateFor(this.tlsConfig.CommonName); err != nil {
-
+		c := this.tlsConfig
+		if this.certificate, err = this.CreatedCertificateFor(c.Organization, c.CommonName, true, nil); err != nil {
+			panic(err)
 		}
+		Must(this.certificate.WriteToFile(c.CACertificateFilePath))
 	}
 }
 
 func (this *CertificateAuthority) GeneratePrivateKey(keyLen int) (*PrivateKey, error) {
-	pair, err := rsa.GenerateKey(rand.Reader,keyLen)
+	pair, err := rsa.GenerateKey(rand.Reader, keyLen)
 	if err != nil {
 		return nil, err
 	}
@@ -134,4 +170,3 @@ func (this *CertificateAuthority) GeneratePrivateKey(keyLen int) (*PrivateKey, e
 		rsa: pair,
 	}, nil
 }
-
--- common.go
+++ common.go
@@ -4,6 +4,17 @@ import (
 	"bytes"
 )
 
-func NewSliceWriter () *bytes.Buffer {
+func NewSliceWriter() *bytes.Buffer {
 	return &bytes.Buffer{}
+}
+
+func Must(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func Must2(r interface{}, err error) interface{} {
+	Must(err)
+	return r
 }
